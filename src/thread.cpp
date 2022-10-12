cdac3d4e116e3d31944055400d97155e06816ee9
am5083 <amohamed5085@icloud.com>
2022-10-12 04:14:31 -0500

    readd TC to QS

--- src/thread.cpp
+++ src/thread.cpp
@@ -109,8 +109,8 @@ int Search::quiescent(int alpha, int beta) {
     selDepth = std::max(selDepth, ply);
     nodes++;
 
-    // if (checkForStop())
-    //     return ABORT_SCORE;
+    if (checkForStop())
+        return ABORT_SCORE;
 
     pvTableLen[ply] = 0;
 
