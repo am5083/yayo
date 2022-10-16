0a5456039a58eb62c87fdaf65a094981f9151210
am5083 <amohamed5085@icloud.com>
2022-10-16 00:41:15 -0500

    re-add real nps to bench; reset tc movestogo

--- src/uci.cpp
+++ src/uci.cpp
@@ -112,8 +112,8 @@ void UCI::Bench() {
     std::uint64_t end_time = get_time();
     long double total_time = 1.0 * (end_time - start_time) / 1000.0;
 
-    std::cout << total_nodes << " nodes " << int(3000000) << " nps"
-              << std::endl;
+    std::cout << total_nodes << " nodes " << (total_nodes / total_time)
+              << " nps" << std::endl;
 }
 
 void UCI::Uci() {
@@ -248,7 +248,7 @@ void UCI::Main() {
             search._make(m);
         } else if (cmd == "go") {
             int depth = 256;
-            int movestogo = 15;
+            int movestogo = 30;
             int movetime = -1;
             int time = -1;
             int increment = 0;
