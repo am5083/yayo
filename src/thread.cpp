fa4232712753892a29bee56828639c51e9b25cda
am5083 <amohamed5085@icloud.com>
2022-10-31 18:47:12 -0500

    adjust delta pruning margin

--- src/thread.cpp
+++ src/thread.cpp
@@ -180,8 +180,10 @@ int Search::quiescent(int alpha, int beta) {
     int queenValue = (eval.mgPhase * MgScore(queenScore) +
                       eval.egPhase * EgScore(queenScore)) /
                      24;
-    int deltaMargin = best + 200 + queenValue;
-    if (deltaMargin < alpha && eval.egPhase <= 20) {
+
+    int deltaMargin = best + queenValue;
+
+    if (deltaMargin < alpha) {
         return alpha;
     }
 
