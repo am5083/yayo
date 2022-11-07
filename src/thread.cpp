d217c0877c0accd5c961dec63fb3c992f7c8a9de
am5083 <amohamed5085@icloud.com>
2022-11-07 02:33:45 -0600

    revert last commit; adjust razoring

--- src/thread.cpp
+++ src/thread.cpp
@@ -319,9 +319,7 @@ int Search::negaMax(int alpha, int beta, int depth, bool nullMove, bool isPv,
 
     if ((ttScore < evalScore && flag == TP_BETA) ||
         (ttScore > evalScore && flag == TP_ALPHA) || flag == TP_EXACT) {
-        if (!pvNode && !_board.checkPcs && depth <= 1 &&
-            ttScore + futilityMargin[2] + 250 < alpha)
-            return quiescent(alpha, beta);
+        evalScore = ttScore;
     }
 
     if (!pvNode && !_board.checkPcs && depth <= 1 &&
@@ -330,7 +328,7 @@ int Search::negaMax(int alpha, int beta, int depth, bool nullMove, bool isPv,
 
     // static NMP
     if (!pvNode && !_board.checkPcs && depth <= 8 &&
-        evalScore - 75 * depth > beta && std::abs(alpha) < INF / 2 &&
+        evalScore - 75 * depth > beta && std::abs(alpha) < 9000 &&
         std::abs(ttScore) < INF / 2)
         return evalScore;
 
@@ -352,7 +350,7 @@ int Search::negaMax(int alpha, int beta, int depth, bool nullMove, bool isPv,
     generate(_board, &mList);
     scoreMoves(&mList, ttMove);
 
-    if (depth <= 3 && !pvNode && std::abs(alpha) < INF / 2 &&
+    if (depth <= 3 && !pvNode && std::abs(alpha) < 9000 &&
         evalScore + futilityMargin[depth] <= alpha && mList.nMoves > 0)
         futilityPrune = true;
 
@@ -377,7 +375,7 @@ int Search::negaMax(int alpha, int beta, int depth, bool nullMove, bool isPv,
         }
 
         int skip = 0;
-        if (_board.ply > 0 && best > -INF && std::abs(alpha) < INF / 2) {
+        if (_board.ply > 0 && best > -INF && std::abs(alpha) < 9000) {
             if (getCapture(curr_move) < CAPTURE) {
                 int reducedDepth =
                       lmrDepthReduction[std::min(63, depth)]
@@ -425,9 +423,8 @@ int Search::negaMax(int alpha, int beta, int depth, bool nullMove, bool isPv,
             score =
                   -negaMax(-beta, -alpha, depth - 1, false, false, isExtension);
         } else {
-            if (std::abs(alpha) < INF / 2 &&
-                movesSearched >= (1 + (2 * isPv)) && depth >= 3 &&
-                getCapture(curr_move) < CAPTURE) {
+            if (std::abs(alpha) < 9000 && movesSearched >= (1 + (2 * isPv)) &&
+                depth >= 3 && getCapture(curr_move) < CAPTURE) {
                 // int R = 2 + (depth / 10);
                 // R += movesSearched / 15;
                 R = lmrDepthReduction[std::min(63, depth)]
